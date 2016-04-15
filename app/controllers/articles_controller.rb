b058597c26c16eda15a69853b39641b1ce120a56
alexlecco <alexlecco24@gmail.com>
2016-04-15 03:33:21 -0300

    [callbacks in article controller and model added]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  #before_action :validate_user, except: [:show, :index]
+  before_action :authenticate_user!, only: [:create, :new]
+  before_action :set_article, except: [:index, :new, :create]
 
   #GET /articles
   def index
@@ -7,7 +10,8 @@ class ArticlesController < ApplicationController
 
   #GET /articles/:id
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
+    @article.update_visits_count
   end
 
   #GET /articles/new
@@ -16,7 +20,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   #POST /articles
@@ -30,7 +34,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
@@ -38,7 +42,7 @@ class ArticlesController < ApplicationController
   #PUT /articles/:id
   def update
     # @article.update_attributes({ title: 'Nuevo titulo' })
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -46,7 +50,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def validate_user
+    redirect_to new_user_session_path, notice: "Necesitas iniciar sesión"
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
